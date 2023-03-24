bae3332ed4c8b1937663cdc879c59640ee16a924
Elijah <elijah.ivanik@inbox.ru>
2023-03-24 12:55:15 +0300

    refactor: replace JSON data with MockAPI

--- src/App.js
+++ src/App.js
@@ -3,10 +3,29 @@ import { Header } from "./components/Header";
 import { Categories } from "./components/Categories";
 import { Sort } from "./components/Sort";
 import { PizzaItem } from "./components/PizzaItem";
-import pizzas from "./model/pizzas.json";
 import "./assets/scss/app.scss";
 
 function App() {
+  const [items, setItems] = React.useState([]);
+
+  React.useEffect(() => {
+    const getItems = async () => {
+      const result = await fetch(
+        "https://641d6897b556e431a8831fcb.mockapi.io/api/v1/items",
+        {
+          headers: {
+            "Access-Control-Allow-Origin": "*",
+            "Content-Type": "application/json"
+          }
+        }
+      );
+      const items = await result.json();
+      setItems(items);
+    };
+
+    getItems();
+  }, []);
+
   return (
     <div className="wrapper">
       <Header />
@@ -19,7 +38,7 @@ function App() {
           </div>
           <h2 className="content__title">Все пиццы</h2>
           <div className="content__items">
-            {pizzas.map((pizza) => (
+            {items.map((pizza) => (
               <PizzaItem key={pizza.id} {...pizza} />
             ))}
           </div>
